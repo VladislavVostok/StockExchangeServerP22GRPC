02a9c8af6e5d5b327abd8b957a07986e10d2fcdf
Vladislav East <vladislav.sage@yandex.ru>
2025-11-14 19:05:39 +0300

    Refactoring generate gRPC files *.ts and *.js

--- stock.exchange.react/scripts/generate-proto.js
+++ stock.exchange.react/scripts/generate-proto.js
@@ -1,23 +1,53 @@
 const { execSync } = require("child_process");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
-const PROTO_PATH = path.join(__dirname, "../proto/stock.proto");
-const OUT_DIR = path.join(__dirname, "../src/types");
+const outDir = path.resolve(__dirname, "../src/types");
+if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true });
+}
 
-console.log(__dirname);
+const protocCmd = [
+    // Путь к бинарному файлу protoc
+    `"${path.resolve(__dirname, "../node_modules/.bin/grpc_tools_node_protoc.cmd")}"`,
 
-const command = `
-protoc \
-    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-    --js_out=import_style=commonjs,binary:${OUT_DIR} \
-    --ts-out=service=grpc-web:${OUT_DIR} \
-    -I ../proto 
-    ${PROTO_PATH}
-`
+    // Путь к плагину TS
+    `--plugin="protoc-gen-ts=${path.resolve(__dirname, "../node_modules/.bin/protoc-gen-ts.cmd")}"`,
 
-try{
-    execSync(command, {stdio:'inherit'});
-    console.log("Gen success!");
-}catch(error){
-    console.log("Gen error!", error);
+    // Путь для подключения proto-файлов
+    `-I "${path.resolve(__dirname, "../proto")}"`,
+
+    // Путь для подключения стандартных типов Google
+    `-I "${path.resolve(__dirname, "../node_modules/google-protobuf")}"`,
+
+    // Настройки вывода JS
+    `--js_out="import_style=commonjs,binary:${outDir}"`,
+
+    // Настройки вывода TS
+    `--ts_out="service=grpc-web:${outDir}"`,
+
+    // Путь к proto-файлу
+    `"${path.resolve(__dirname, "../proto/stock.proto")}"`
+].join(" ");
+
+
+try {
+    console.log(`Очищаем директорию: ${outDir}`);
+    fs.readdirSync(outDir).forEach(f => fs.rmSync(`${outDir}/${f}`));
+    
+    console.log("Выполняем команду...");
+    console.log(protocCmd);
+    execSync(protocCmd, { stdio: 'inherit' });
+
+    const generatedFiles = fs.readdirSync(outDir);
+    if (generatedFiles.length > 0) {
+        console.log("Генерация файлов прошла успешно:");
+        console.log(generatedFiles.join("\n"));
+    } else {
+        throw new Error("Файлы не сгенерированы.");
+    }
+
+} catch (error) {
+    console.error("Ошибка генерации файлов:", error);
+    process.exit(1);
 }
